9f096046bdfb0f58d77319490c1b04bfd1d2d3c6
Guilherme Costa <costaguilherme1995@gmail.com>
2023-07-24 15:01:39 +0200

    restructing the folders. Adding the filter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./componentsproject/Footer";
-import Sidebar from "./componentsproject/Sidebar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 import "./index.css";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { useContext, useState } from "react";
 import { ProductContext } from "../contexts/ProductContext";
 import { Button } from "flowbite-react";
+
 const Home = () => {
   const products = useContext(ProductContext);
   const [selectedCategory, setSelectedCategory] = useState<string>("");
+
+  console.log(products);
 
   const filterCategories = products.filter((product) => {
     return selectedCategory === "" || product.category === selectedCategory;
@@ -21,17 +24,36 @@ const Home = () => {
           max-w-sm mx-auto md:max-w-none md:mx-0"
           >
             <div className="flex justify-between">
-              <Button gradientDuoTone="purpleToBlue" outline>
+              <Button
+                onClick={() => handleCategory("")}
+                gradientDuoTone="purpleToBlue"
+                outline
+              >
+                All
+              </Button>
+              <Button
+                onClick={() => handleCategory("women's clothing")}
+                gradientDuoTone="purpleToBlue"
+                outline
+              >
                 Women's
               </Button>
-              <Button gradientDuoTone="purpleToBlue" outline>
+              <Button
+                onClick={() => handleCategory("men's clothing")}
+                gradientDuoTone="purpleToBlue"
+                outline
+              >
                 Mens
               </Button>
-              <Button gradientDuoTone="purpleToBlue" outline>
+              <Button
+                onClick={() => handleCategory("jewelery")}
+                gradientDuoTone="purpleToBlue"
+                outline
+              >
                 Jewlery
               </Button>
             </div>
-            {products.map((product) => (
+            {filterCategories.map((product) => (
               <div key={product.id}>{product.title}</div>
             ))}
           </div>
