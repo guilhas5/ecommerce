979d85c5231c8a403ff7cbdcb826b9c325e5b9b8
Guilherme Costa <costaguilherme1995@gmail.com>
2023-07-28 22:57:19 +0200

    styling btns Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { ProductContext } from "../contexts/ProductContext";
 import { Button } from "flowbite-react";
 import Product from "../components/Product";
+import Hero from "../components/Hero";
 
 const Home = () => {
   const products = useContext(ProductContext);
@@ -16,38 +17,45 @@ const Home = () => {
   };
   return (
     <div>
+      <Hero />
       <section className="py-20">
             <div className="flex justify-center gap-5">
               <Button
                 onClick={() => handleCategory("")}
-                gradientDuoTone="purpleToBlue"
+                gradientDuoTone="cyanToBlue"
                 outline
               >
                 All
               </Button>
               <Button
                 onClick={() => handleCategory("women's clothing")}
-                gradientDuoTone="purpleToBlue"
+                gradientDuoTone="cyanToBlue"
                 outline
               >
                 Women's
               </Button>
               <Button
                 onClick={() => handleCategory("men's clothing")}
-                gradientDuoTone="purpleToBlue"
+                gradientDuoTone="cyanToBlue"
                 outline
               >
                 Mens
               </Button>
               <Button
                 onClick={() => handleCategory("jewelery")}
-                gradientDuoTone="purpleToBlue"
+                gradientDuoTone="cyanToBlue"
                 outline
               >
                 Jewlery
               </Button>
+              <Button
+                onClick={() => handleCategory("electronics")}
+                gradientDuoTone="cyanToBlue"
+                outline
+              >
+                Electronics
+              </Button>
             </div>
-      
         <div className="container mx-auto">
             <div
             className="pt-12 grid center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-7
