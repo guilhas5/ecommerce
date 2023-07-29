b6dbb7162330c63ecb58d52c301baf7cdf900c8d
Guilherme Costa <costaguilherme1995@gmail.com>
2023-07-29 13:38:10 +0200

    Adding the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
-
-
+import { FiInstagram,FiFacebook,FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer className="bg-gradient-to-r from-cyan-500 to-blue-400 py-12">
+      <div className="container mx-auto">
+        <div className="flex justify-center gap-x-5 mb-2 text-lg cursor-pointer">
+        <FiInstagram  />
+        <FiFacebook />
+        <FiTwitter />
+        </div>
+        <p className="text-center font-semibold">Copyright &copy; Ecommerce Shop data. All rights reserved {currentYear}</p>
+      </div>
+      </footer>
   )
 }
 
